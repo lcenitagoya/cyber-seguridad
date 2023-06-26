2f4f833ac63f11fbc904d658774ccc997e042176
Mirena Torres Kiehr <112397106+mirutk@users.noreply.github.com>
2023-06-26 20:46:40 -0300

    form + seguridad

--- procesar.formulario.php
+++ procesar.formulario.php
@@ -1,3 +1,10 @@
 <?php
-print_r($_POST);
-?>
+
+$edad = $_POST["edad"];
+$antivirus = $_POST["antivirus"];
+$contraseña = $_POST["contraseña"];
+$informacion = $_POST["informacion"];
+$ataques = $_POST["ataques"];
+$cambioCib = $_POST["cambioCib"];
+
+var_dump($edad, $antivirus, $contraseña, $informacion, $ataques, $cambioCib);
