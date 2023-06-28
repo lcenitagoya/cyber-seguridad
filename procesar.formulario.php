8cc59db893b8c552f6bd368596fc7e2ec0bbb36a
Mirena Torres Kiehr <112397106+mirutk@users.noreply.github.com>
2023-06-28 01:08:52 -0300

    localhost

--- procesar.formulario.php
+++ procesar.formulario.php
@@ -8,41 +8,18 @@ $informacion = filter_input(INPUT_POST, "informacion", FILTER_VALIDATE_BOOL);
 $ataques = filter_input(INPUT_POST, "ataques", FILTER_VALIDATE_BOOL);
 $cambioCib = filter_input(INPUT_POST, "cambioCib", FILTER_VALIDATE_BOOL);
 
-if ( ! $terms) {
-    die("Terms must be accepted");
-}   
+
 
 $host = "localhost";
-$dbname = "message_db";
+$dbname = "ciberseguridad";
 $username = "root";
 $password = "";
-        
-$conn = mysqli_connect(hostname: $host,
-                       username: $username,
-                       password: $password,
-                       database: $dbname);
-        
+
+$conn = new mysqli("localhost", "root", "", "ciberseguridad");
+
 if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
 }           
-        
-$sql = "INSERT INTO message (name, body, priority, type)
-        VALUES (?, ?, ?, ?)";
 
-$stmt = mysqli_stmt_init($conn);
-
-if ( ! mysqli_stmt_prepare($stmt, $sql)) {
- 
-    die(mysqli_error($conn));
-}
-
-mysqli_stmt_bind_param($stmt, "ssii",
-                       $name,
-                       $message,
-                       $priority,
-                       $type);
-
-mysqli_stmt_execute($stmt);
-
-echo "Record saved.";
-?>
+echo "yaaas.";
+                            
